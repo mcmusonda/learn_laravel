b1b4eec99013828724c9f890c04ca0235ead5e4f
Chali Musonda <51807287+mcmusonda@users.noreply.github.com>
2025-03-21 15:22:49 +0200

    Initial project commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot>
+        <h2>Contact us</h2>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot>
+    <h2>Home Page</h2>
+</x-layout>
